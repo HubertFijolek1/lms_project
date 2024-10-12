2856956df2ef0458dad89a8d8de41b7911ecd84b
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-10-12 15:39:21 +0200

    Remove unnecessary comments in courses/models.py
    This commit removes redundant comment lines at the top of the `courses/models.py` file. This cleanup helps improve the readability of the code and adheres to code quality standards.

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,6 @@
-### courses/models.py
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
