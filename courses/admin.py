639527ef56dc4a62862c6e60a2a875e4765931ac
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-10-14 14:40:59 +0200

    Refactor admin.py and update CourseAdmin
    Consolidated CourseAdmin by directly registering with @admin.register. Added filter_horizontal for prerequisites and students to improve the admin interface. Removed the BaseAdmin class, making ScheduleAdmin inherit directly from admin.ModelAdmin.

--- courses/admin.py
+++ courses/admin.py
@@ -1,21 +1,15 @@
-### courses/admin.py
-
 from django.contrib import admin
 from .models import Course, Schedule
 
-
-class BaseAdmin(admin.ModelAdmin):
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'credits', 'maximum_capacity', 'availability')
     search_fields = ('name', 'description')
     list_filter = ('availability',)
-
-
-@admin.register(Course)
-class CourseAdmin(BaseAdmin):
-    list_display = ('name', 'description', 'availability')
-
+    filter_horizontal = ('prerequisites', 'students')  # For better UI in admin
 
 @admin.register(Schedule)
-class ScheduleAdmin(BaseAdmin):
+class ScheduleAdmin(admin.ModelAdmin):
     list_display = ('course', 'user', 'start_time', 'end_time')
     search_fields = ('course__name', 'user__username')
     list_filter = ('course', 'start_time', 'end_time')
