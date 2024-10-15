6d395aa54e67b4ed9e23b5e88a8be3bd298088a6
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-10-15 15:14:01 +0200

    Refactor logout to clear session correctly
    Removed unnecessary cache control imports and simplified `CustomLogoutView` dispatch method to ensure proper session flushing during logout. This results in cleaner and more efficient code, improving maintainability.

--- lms_project/views.py
+++ lms_project/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from allauth.account.views import LogoutView
-from django.views.decorators.cache import never_cache, cache_control
-
 
 # Helper function to clear session and logout
 def logout_and_flush_session(request):
     logout(request)  # Ensures the user session is properly cleared.
     request.session.flush()  # Flushes the session data.
-
-
 def home(request):
     return render(request, 'home.html')
-
 
 class CustomLogoutView(LogoutView):
     def dispatch(self, request, *args, **kwargs):
